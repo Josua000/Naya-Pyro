7f1a3e3294a6a37e2623ec50ec5e37ebbebafc07
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-17 13:56:46 +0700

    update __init__.py

--- naya/__init__.py
+++ naya/__init__.py
@@ -13,14 +13,12 @@ from pyrogram import Client, __version__, enums, filters
 from pyrogram.handlers import MessageHandler
 from pyromod import listen
 from pytgcalls import GroupCallFactory
+#from telegraph import Telegraph, exceptions, upload_file
 
 from .config import (API_HASH, API_ID, BOT_TOKEN, CMD_HNDLR, SESSION1,
                      SESSION2, SESSION3, SESSION4, SESSION5, SESSION6,
                      SESSION7, SESSION8, SESSION9, SESSION10)
 from .logging import LOGGER
-
-# from telegraph import Telegraph, exceptions, upload_file
-
 
 StartTime = time.time()
 cmd = CMD_HNDLR
@@ -28,8 +26,8 @@ ids = []
 scheduler = AsyncIOScheduler()
 CMD_HELP = {}
 START_TIME = datetime.now()
-# telegraph = Telegraph()
-# telegraph.create_account(short_name="ubot")
+telegraph = Telegraph()
+telegraph.create_account(short_name="ubot")
 
 aiosession = ClientSession()
 
@@ -44,7 +42,6 @@ app = Client(
 if not BOT_TOKEN:
     LOGGER(__name__).error("WARNING: BOT TOKEN TIDAK DITEMUKAN, SHUTDOWN BOT")
     sys.exit()
-
 
 class Ubot(Client):
     _bots = []
@@ -65,7 +62,6 @@ class Ubot(Client):
         await super().start()
         if self not in self._bots:
             self._bots.append(self)
-
 
 bot1 = (
     Ubot(
