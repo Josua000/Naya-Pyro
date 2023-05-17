62348087877e099a69edd236371a890088671b54
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-17 16:11:48 +0700

    update spam.py

--- naya/modules/spam.py
+++ naya/modules/spam.py
@@ -21,7 +21,7 @@ async def delayspammer(client, message):
         delay = float(args[1])
         count = int(args[2])
         if message.reply_to_message:
-            msg = client.get_message()
+            msg = client.get_reply_message
         else:
             msg = str(args[3])
     except BaseException:
