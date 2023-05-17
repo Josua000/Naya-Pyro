0868b69337eebcdfec1f1277bf8422a2fbd8c7d6
naya1503 <naya1503@users.noreply.github.com>
2023-05-17 20:48:59 +0000

    Auto Fix

--- naya/modules/help.py
+++ naya/modules/help.py
@@ -295,10 +295,10 @@ async def close(_, query: CallbackQuery):
             )
         else:
             await query.message.delete()
-            buttons=[
-                        [InlineKeyboardButton(text="Kembali", callback_data="multi")],
-                        [InlineKeyboardButton("Tutup", callback_data="cl_ad")],
-                    ]
+            buttons = [
+                [InlineKeyboardButton(text="Kembali", callback_data="multi")],
+                [InlineKeyboardButton("Tutup", callback_data="cl_ad")],
+            ]
             return await app.send_message(
                 user_id,
                 f"**Berhasil Menambahkan var `{to_set}` menjadi `{value}`**",
