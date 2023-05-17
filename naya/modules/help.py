21d1eb3fe0add11751c6f683f9ce364ae29d5c58
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-18 03:46:13 +0700

    update help.py

--- naya/modules/help.py
+++ naya/modules/help.py
@@ -296,10 +296,10 @@ async def close(_, query: CallbackQuery):
             )
         else:
             await query.message.delete()
-            buttons = [
-                [InlineKeyboardButton(text="Kembali", callback_data="multi")],
-                [InlineKeyboardButton("Tutup", callback_data="cl_ad")],
-            ]
+            buttons=[
+                        [InlineKeyboardButton(text="Kembali", callback_data="multi")],
+                        [InlineKeyboardButton("Tutup", callback_data="cl_ad")],
+                    ]
             return await app.send_message(
                 user_id,
                 f"**Berhasil Menambahkan var `{to_set}` menjadi `{value}`**",
@@ -405,6 +405,7 @@ async def close(_, query: CallbackQuery):
 
 @app.on_callback_query(filters.regex("multi"))
 async def close(_, query: CallbackQuery):
+    await query.message.delete()
     await query.edit_message_text(
         "<b>Kamu bisa mengatur SESSION1-10, dan kamu juga bisa menghapus nya.</b>",
         reply_markup=InlineKeyboardMarkup(
