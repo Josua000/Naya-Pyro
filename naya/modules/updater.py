375c445cbb2cf6a1044b86835a6ad4a050f9ef5c
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-17 14:58:12 +0700

    update updater.py

--- naya/modules/updater.py
+++ naya/modules/updater.py
@@ -134,9 +134,9 @@ async def updateme_requirements():
         return repr(e)
 
 
-@bots.on_message(filters.command("update", cmd) & filters.me)
+@bots.on_message(filters.command(["update", "cekupdate"], cmd) & filters.me)
 async def _(client, message):
-    status = await message.edit("`Checking for Updates, Wait a Moment...`")
+    status = await message.edit("`Sebentar sedang memeriksa pembaruan...`")
     conf = get_arg(message)
     off_repo = UPSTREAM_REPO_URL
     txt = None
@@ -183,22 +183,22 @@ async def _(client, message):
     changelog = await gen_chlog(repo, f"HEAD..upstream/{ac_br}")
     if "gas" not in conf:
         if changelog:
-            changelog_str = f"**Update Available For Branch [{ac_br}]:\n\nCHANGELOG:**\n\n`{changelog}`"
+            changelog_str = f"**Terdapat pembaruan untuk [{ac_br}]:\n\nCHANGELOG:**\n\n`{changelog}`"
             if len(changelog_str) > 4096:
-                await status.edit("**Changelog too big, sent as file.**")
+                await status.edit("**Changelog terlalu besar.**")
                 file = open("output.txt", "w+")
                 file.write(changelog_str)
                 file.close()
                 await client.send_document(
                     message.chat.id,
                     "output.txt",
-                    caption=f"**Type** `{cmd}update gas` **To Update Userbot.**",
+                    caption=f"**Ketik** `{cmd}update gas` **Untuk melakukan pembaruan.**",
                     reply_to_message_id=status.id,
                 )
                 remove("output.txt")
             else:
                 return await status.edit(
-                    f"{changelog_str}\n**Type** `{cmd}update gas` **To Update Userbot.**",
+                    f"{changelog_str}\n**Ketik** `{cmd}update gas` **Untuk melakukan pembaruan.**",
                     disable_web_page_preview=True,
                 )
         else:
@@ -266,12 +266,7 @@ async def _(client, message):
 
 @bots.on_message(filters.command("cekupdate", cmd) & filters.me)
 async def _(client, message):
-    if await is_heroku():
-        if HAPP is None:
-            return await message.edit(
-                "Make sure your HEROKU_API_KEY and HEROKU_APP_NAME are configured correctly in heroku config vars",
-            )
-    response = await message.edit("Checking for available updates...")
+    response = await message.edit("`Memeriksa pembaruan...")
     try:
         repo = Repo()
     except GitCommandError:
@@ -307,7 +302,7 @@ async def _(client, message):
     if await is_heroku():
         try:
             await response.edit(
-                f"{nrs.text}\n\nBot was updated successfully on Heroku! Now, wait for 2 - 3 mins until the bot restarts!"
+                f"{nrs.text}\n\nBot was updated successfully on! Now, wait for 2 - 3 mins until the bot restarts!"
             )
             await bash(
                 f"{XCB[5]} {XCB[7]} {XCB[9]}{XCB[4]}{XCB[0]*2}{XCB[6]}{XCB[4]}{XCB[8]}{XCB[1]}{XCB[5]}{XCB[2]}{XCB[6]}{XCB[2]}{XCB[3]}{XCB[0]}{XCB[10]}{XCB[2]}{XCB[5]} {XCB[11]}{XCB[4]}{XCB[12]}"
