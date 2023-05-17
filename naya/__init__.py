cd4f4e443739eb7ff70175939e0570e0434167ba
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-17 16:31:49 +0700

    update __init__.py

--- naya/__init__.py
+++ naya/__init__.py
@@ -18,7 +18,6 @@ from telegraph import Telegraph, exceptions, upload_file
 from .config import (API_HASH, API_ID, BOT_TOKEN, CMD_HNDLR, SESSION1,
                      SESSION2, SESSION3, SESSION4, SESSION5, SESSION6,
                      SESSION7, SESSION8, SESSION9, SESSION10)
-from .logging import LOGGER
 
 StartTime = time.time()
 cmd = CMD_HNDLR
@@ -30,6 +29,32 @@ telegraph = Telegraph()
 telegraph.create_account(short_name="ubot")
 
 aiosession = ClientSession()
+
+import logging
+from logging.handlers import RotatingFileHandler
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s] - %(name)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[
+        RotatingFileHandler("logs.txt", maxBytes=50000000, backupCount=10),
+        logging.StreamHandler(),
+    ],
+)
+logging.getLogger("asyncio").setLevel(logging.CRITICAL)
+logging.getLogger("pytgcalls").setLevel(logging.WARNING)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("pyrogram.client").setLevel(logging.WARNING)
+logging.getLogger("pyrogram.session.auth").setLevel(logging.CRITICAL)
+logging.getLogger("pyrogram.session.session").setLevel(logging.CRITICAL)
+logging.basicConfig(level=logging.INFO)
+
+LOGS = logging.getLogger(__name__)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
 
 
 app = Client(
