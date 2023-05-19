ef4f5fbae87a0d4532434e18b68c950b453d5582
naya1503 <naya1503@users.noreply.github.com>
2023-05-19 08:19:38 +0000

    Auto Fix

--- naya/__init__.py
+++ naya/__init__.py
@@ -6,10 +6,12 @@ import time
 from datetime import datetime
 from logging.handlers import RotatingFileHandler
 from typing import Any, Dict
-from pyrogram import Client as bots
+
 from aiohttp import ClientSession
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from pyrogram import Client, __version__, enums, filters
+from pyrogram import Client
+from pyrogram import Client as bots
+from pyrogram import __version__, enums, filters
 from pyrogram.handlers import MessageHandler
 from pyromod import listen
 from pytgcalls import GroupCallFactory
