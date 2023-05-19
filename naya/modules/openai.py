79d65e5bc5e1b519ad9979b240c789f09c814cb1
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-19 08:49:52 +0700

    update openai.py

--- naya/modules/openai.py
+++ naya/modules/openai.py
@@ -5,7 +5,7 @@ from pyrogram import filters
 from pyrogram.errors import *
 from pyrogram.errors import MessageNotModified
 from pyrogram.types import *
-
+from random import choices
 from naya import cmd
 from naya.config import OPENAI_API
 
