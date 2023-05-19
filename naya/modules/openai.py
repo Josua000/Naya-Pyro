1bfc10a0c9693007de05b02832f946cd16b1308a
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-19 10:41:04 +0700

    update openai.py

--- naya/modules/openai.py
+++ naya/modules/openai.py
@@ -4,13 +4,30 @@ from kynaylibs.nan.utils.http import *
 from pyrogram import filters
 from pyrogram.errors import MessageNotModified
 from pyrogram.types import *
-
+import openai
 from naya import cmd
 from naya.config import OPENAI_API
 
-from . import *
+class OpenAi:
+    def Text(question):
+        openai.api_key = OPENAI_API
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=f"Q: {question}\nA:",
+            temperature=0,
+            max_tokens=500,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+        )
+        return response.choices[0].text
 
-
+    def Photo(question):
+        openai.api_key = OPENAI_API
+        response = openai.Image.create(prompt=question, n=1, size="1024x1024")
+        return response["data"][0]["url"]
+        
+        
 @bots.on_message(filters.me & filters.command(["ai", "ask"], cmd))
 async def ai(client, message):
     if len(message.command) == 1:
