60fc5d33fc77b710b11f4866bd6f68ffbcdc46bf
naya1503 <naya1503@users.noreply.github.com>
2023-05-20 23:34:41 +0000

    Auto Fix

--- naya/modules/__init__.py
+++ naya/modules/__init__.py
@@ -1,13 +1,15 @@
+import asyncio
 from glob import glob
 from os.path import basename, dirname, isfile
-from requests import get
+
 from kynaylibs import DEVS
 from kynaylibs.nan import *
 from kynaylibs.nan.utils import *
 from kynaylibs.nan.utils.db import *
 from pyrogram import *
 from pyrogram.types import *
-import asyncio
+from requests import get
+
 from naya import *
 
 BL_UBOT = [-1001812143750]
@@ -19,12 +21,21 @@ while 0 < 6:
     if _BL_GCAST.status_code != 200:
         if 0 != 5:
             continue
-        BL_GCAST = [-1001812143750, -1001473548283, -1001390552926, -1001573099403, -1001810928340, -1001619428365, -1001825363971]
+        BL_GCAST = [
+            -1001812143750,
+            -1001473548283,
+            -1001390552926,
+            -1001573099403,
+            -1001810928340,
+            -1001619428365,
+            -1001825363971,
+        ]
         break
     BL_GCAST = _BL_GCAST.json()
     break
 
 del _BL_GCAST
+
 
 def loadModule():
     mod_paths = glob(f"{dirname(__file__)}/*.py")
