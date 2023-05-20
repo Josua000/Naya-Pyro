9b313a0240d34d579d56f88c996404be2aac4e48
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-20 17:31:34 +0700

    update help.py

--- naya/modules/help.py
+++ naya/modules/help.py
@@ -109,22 +109,24 @@ async def _(client, inline_query):
 
 @app.on_inline_query(filters.regex("^user_help_command"))
 async def _(client, inline_query):
-    msg = f"<b>❏ Owner : {client.me.mention}\n✘ Menu Bantuan\n๏ Perintah: <code>{cmd}</code></b>"
-    await client.answer_inline_query(
-        inline_query.id,
-        cache_time=0,
-        results=[
-            (
-                InlineQueryResultArticle(
+  for x in botlist:
+    try:
+        msg = f"<b>❏ Owner : <a href=tg://user?id={x.me.id}>{X.me.first_name} {x.me.last_name or ''}</a>\n✘ Menu Bantuan\n๏ Perintah: <code>{cmd}</code></b>",
+        await client.answer_inline_query(
+            inline_query.id,
+            cache_time=0,
+            results=[
+                (
+                  InlineQueryResultArticle(
                     title="Help Menu!",
                     reply_markup=InlineKeyboardMarkup(
-                        paginate_modules(0, CMD_HELP, "help")
-                    ),
-                    input_message_content=InputTextMessageContent(msg),
-                )
-            )
-        ],
-    )
+                      paginate_modules(0, CMD_HELP, "help")
+                      ),
+                      input_message_content=InputTextMessageContent(msg),
+                    )
+                  )
+                ],
+              )
 
 
 @app.on_callback_query(filters.regex(r"help_(.*?)"))
@@ -142,7 +144,7 @@ async def _(client, callback_query):
             reply_markup=InlineKeyboardMarkup(button),
             disable_web_page_preview=True,
         )
-    top_text = f"<b>❏ Owner : {callback_query.bots.me.mention}\n✘ Menu Bantuan\n๏ Perintah: <code>{cmd}</code></b>"
+    top_text = f"<b>❏ Owner : \n✘ Menu Bantuan\n๏ Perintah: <code>{cmd}</code></b>"
     if prev_match:
         curr_page = int(prev_match.group(1))
         await callback_query.edit_message_text(
