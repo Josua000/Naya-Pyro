d723972bd5a0e4fcd8ab8d54e39a16e738576a66
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-06-09 01:42:27 +0700

    update nulis.py

--- naya/modules/nulis.py
+++ naya/modules/nulis.py
@@ -6,23 +6,55 @@
 # FULL MONGO NIH JING FIX MULTI CLIENT
 
 
-import requests
+import glob
+import os
+import random
+from PIL import *
 
 from . import *
 
-
+def text_set(text):
+    lines = []
+    if len(text) <= 55:
+        lines.append(text)
+    else:
+        all_lines = text.split("\n")
+        for line in all_lines:
+            if len(line) <= 55:
+                lines.append(line)
+            else:
+                k = len(line) // 55
+                lines.extend(line[((z - 1) * 55) : (z * 55)] for z in range(1, k + 2))
+    return lines[:25]
+    
 @bots.on_message(filters.command(["nulis", "nl"], cmd) & filters.me)
 async def handwrite(client, message):
     if message.reply_to_message:
         naya = message.reply_to_message.text
     else:
         naya = message.text.split(None, 1)[1]
-    nan = await message.reply("`Processing...`")
-    ajg = requests.get(f"https://api.sdbots.tk/write?text={naya}").url
-    await message.reply_photo(
-        photo=ajg, caption=f"**Ditulis Oleh :** {client.me.mention}"
-    )
-    await nan.delete()
+    nan = await eor(message, "Processing...")
+    try:
+        img = Image.open("naya/resources/kertas.jpg")
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype("naya/resources/fonts/assfont.ttf", 30)
+        x, y = 150, 140
+        lines = text_set(naya)
+        line_height = font.getsize("hg")[1]
+        for line in lines:
+            draw.text((x, y), line, fill=(1, 22, 55), font=font)
+            y = y + line_height - 5
+        file = "nulis.jpg"
+        img.save(file)
+        if os.path.exists(file):
+            await message.reply_photo(
+                photo=file,
+                caption=f"<b>Ditulis Oleh :</b> {client.me.mention}"
+            )
+            os.remove(file)
+            await nan.delete()
+    except Exception as e:
+        return await message.reply(e)
 
 
 __MODULE__ = "nulis"
